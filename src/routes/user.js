d5720ebe126e312b5671e78a893b5a09a278a7ba
oliviatroitino <olivia.troitino@live.u-tad.com>
2025-06-13 19:18:17 +0200

    agrega src/routes/user.js, define el router

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
+//// Validador
+// const { validatorRegister } = require("../src/validators/user");
+//// Controller
+// const { registerCtrl } = require("../src/controllers/user");
 
-// router.post("/register", registerUser);
+// router.post("/register", validatorRegister, registerUser);
 
 module.exports = router;
