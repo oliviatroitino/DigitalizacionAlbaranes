be85da33f5b627d3ba2f343e114ae0052f13bf13
oliviatroitino <olivia.troitino@live.u-tad.com>
2025-06-20 21:56:18 +0200

    agrega getProject

--- src/routes/project.js
+++ src/routes/project.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middleware/session");
 const { validatorCreateProject, validatorGetProjects, validatorGetProject } = require("../validators/project");
-const { createProject, getProjects } = require("../controllers/project");
+const { createProject, getProjects, getProject } = require("../controllers/project");
 
 router.post("/", authMiddleware, validatorCreateProject, createProject);
 router.get("/", authMiddleware, validatorGetProjects, getProjects);
-// router.get("/:id", authMiddleware, validatorGetProject, );
+router.get("/:id", authMiddleware, validatorGetProject, getProject);
 
 module.exports = router;
