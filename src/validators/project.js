d523fc5e77d80dc5fee2dab7e44fa8de821c4bc3
oliviatroitino <olivia.troitino@live.u-tad.com>
2025-06-20 15:58:52 +0200

    agrega validatorGetProjects

--- src/validators/project.js
+++ src/validators/project.js
@@ -2,23 +2,33 @@ const { check } = require("express-validator");
 const { validateResults } = require("../utils/handleValidator");
 
 const validatorCreateProject = [
-  check("name").exists().notEmpty().isString(),
-  check("projectCode").exists().notEmpty().isString(),
-  check("email").exists().notEmpty().isEmail(),
-  check("code").exists().notEmpty().isString(),
+    check("name").exists().notEmpty().isString(),
+    check("projectCode").exists().notEmpty().isString(),
+    check("email").exists().notEmpty().isEmail(),
+    check("code").exists().notEmpty().isString(),
 
-  check("address").exists(),
-  check("address.street").exists().notEmpty().isString(),
-  check("address.number").exists().notEmpty().isNumeric(),
-  check("address.postal").exists().notEmpty().isNumeric(),
-  check("address.city").exists().notEmpty().isString(),
-  check("address.province").exists().notEmpty().isString(),
+    check("address").exists(),
+    check("address.street").exists().notEmpty().isString(),
+    check("address.number").exists().notEmpty().isNumeric(),
+    check("address.postal").exists().notEmpty().isNumeric(),
+    check("address.city").exists().notEmpty().isString(),
+    check("address.province").exists().notEmpty().isString(),
 
-  check("clientId").exists().notEmpty().isMongoId(),
+    check("clientId").exists().notEmpty().isMongoId(),
 
-  (req, res, next) => {
-    return validateResults(req, res, next);
-  }
+    (req, res, next) => {
+      return validateResults(req, res, next);
+    }
 ];
 
-module.exports = { validatorCreateProject };
+const validatorGetProjects = [
+    check("id")
+      .exists().withMessage("ID is required")
+      .notEmpty().withMessage("ID cannot be empty")
+      .isMongoId().withMessage("ID must be a valid Mongo ID"),
+    (req, res, next) => {
+      return validateResults(req, res, next);
+    }
+];
+
+module.exports = { validatorCreateProject, validatorGetProjects };
