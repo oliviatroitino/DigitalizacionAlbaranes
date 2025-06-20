3e1875f08478d18f018b1a8c307e3bbe6291d391
oliviatroitino <olivia.troitino@live.u-tad.com>
2025-06-20 14:57:44 +0200

    agrega updateUser y updateCompany

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const router = express.Router();
 //// Validador
-const { validatorRegisterUser, validatorLogin } = require("../validators/user");
+const { validatorRegisterUser, validatorLogin, validatorUserData, validatorCompanyData } = require("../validators/user");
 const { validatorEmailCode } = require("../validators/email");
 //// Controller
-const { registerUser, verifyEmail, loginUser } = require("../controllers/user");
+const { registerUser, verifyEmail, loginUser, updateUser, updateCompany } = require("../controllers/user");
 const authMiddleware = require("../middleware/session.js");
 
 router.post("/register", validatorRegisterUser, registerUser);
 router.put("/validation", validatorEmailCode, verifyEmail);
 router.post("/login", validatorLogin, loginUser);
-router.patch("/", authMiddleware, validatorUpdateUser, updateUser);
-router.patch("/company", authMiddleware,  validatorCompany, updateCompany);
+router.patch("/", authMiddleware, validatorUserData, updateUser);
+router.patch("/company", authMiddleware,  validatorCompanyData, updateCompany);
 
 module.exports = router;
