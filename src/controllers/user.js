a1fa7b3d94cf0cd7afe20341af0706a7439b9639
oliviatroitino <olivia.troitino@live.u-tad.com>
2025-06-20 14:19:48 +0200

    agrega funcionalidad login al user.js

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,8 +1,9 @@
 const { matchedData } = require("express-validator");
-const { encrypt } = require("../utils/handlePassword");
+const { encrypt, compare } = require("../utils/handlePassword");
 const UserModel = require("../models/nosql/user");
 const { handleHttpError } = require("../utils/handleError");
 const { tokenSign } = require("../utils/handleJwt.js");
+const { sendEmail } = require("../utils/handleEmail.js");
 
 const registerUser = async (req, res) => {
     try {
@@ -10,7 +11,7 @@ const registerUser = async (req, res) => {
         const hashedPassword = await encrypt(body.password);
 
         // ver si ya existe el usuario
-        const existingUser = await UserModel.findOne({ email: body.email, status: true });
+        const existingUser = await UserModel.findOne({ email: body.email });
 
         if (existingUser) {
             // ya existe el email y está validado
@@ -26,22 +27,80 @@ const registerUser = async (req, res) => {
         const newUser = await UserModel.create(body);
 
         await sendEmail({
-            to: user.email,
+            to: newUser.email,
             subject: "Validation code",
-            html: `<p>Your code is <b>${user.emailCode}</b></p>`,
+            html: `<p>Your code is <b>${newUser.emailCode}</b></p>`,
             from: process.env.EMAIL
         });
 
-        const token = await tokenSign(user);
+        const token = await tokenSign(newUser);
         
         newUser.set("password", undefined, {strict: false});
-        newUser.set("emailCode", undefined, {strict: false});
+        // newUser.set("emailCode", undefined, {strict: false});
 
         res.send({ user: newUser , token, message: "User registered, check email."});
     } catch (err) {
-        console.log(err);
+        console.log(`Registration error ${err}`);
         handleHttpError(res, "ERROR_REGISTER_USER", 500);
     }
 }
 
-module.exports = { registerUser };
+const verifyEmail = async (req, res) => {
+    try {
+        const body = matchedData(req);
+
+        // buscar usuario por emai
+        const user = await UserModel.findOne({ email: body.email });
+
+        // comparar body.emailCode con user.emailCode
+        if (!user) return handleHttpError(res, "ERROR_USER_NOT_FOUND", 401);
+
+        if (body.emailCode == user.emailCode){
+            user.status = 1;
+            await user.save();
+        } else {
+            return handleHttpError(res, "ERROR_EMAILCODE_INCORRECT", 401);
+        }
+
+        // enviar email de confirmacion
+        await sendEmail({
+            to: user.email,
+            subject: "User validated successfully",
+            html: `<p>Thank you for registering.</p>`,
+            from: process.env.EMAIL
+        });
+
+        const token = await tokenSign(user);
+
+        user.set("password", undefined, { strict: false });
+        res.send({ user: user , token, message: "User verified."});
+    } catch (err) {
+        console.log(`Verification error ${err}`);
+       return handleHttpError(res, "ERROR_VERIFYING_USER", 500);
+    }
+}
+
+const loginUser = async (req, res) => {
+    const body = matchedData(req);
+    const user = await UserModel.findOne({ email: body.email });
+
+    if(!user){
+        return handleHttpError(res, "ERROR_USER_NOT_FOUND", 404);
+    }
+
+    if (user.status !== 1) {
+        return handleHttpError(res, "USER_NOT_VALIDATED", 401);
+    }
+
+    const correctPassword = await compare(body.password, user.password);
+
+    if(!correctPassword){
+        return handleHttpError(res, "ERROR_INCORRECT_PASSWORD", 401);
+    }
+
+    const token = await tokenSign(user);
+    user.set("password", undefined, { strict: false });
+    res.send({ user, token });
+}
+
+module.exports = { registerUser, verifyEmail, loginUser };
