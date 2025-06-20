00367d752b3830c2ac61011419fede98ce99e625
oliviatroitino <olivia.troitino@live.u-tad.com>
2025-06-20 14:28:56 +0200

    agrega authentication middleware

---
+++ src/middleware/session.js
@@ -0,0 +1,30 @@
+const { handleHttpError } = require("../utils/handleError");
+const UserModel = require("../models/user.js");
+const { verifyToken } = require("../utils/handleJwt.js")
+
+const authMiddleware = async (req, res, next) => {
+    try {
+        if (!req.headers.authorization) {
+            handleHttpError(res, "NOT_TOKEN", 401);
+            return;
+        }
+
+        const token = req.headers.authorization.split(' ').pop();
+
+        const dataToken = await verifyToken(token);
+
+        if (!dataToken._id) {
+            handleHttpError(res, "ERROR_ID_TOKEN", 401);
+            return;
+        }
+
+        const user = await UserModel.findById(dataToken._id)
+
+        req.user = user;
+        next();
+    } catch (err) {
+        handleHttpError(res, "NOT_SESSION", 401);
+    }
+};
+
+module.exports = authMiddleware;
