fe87cd27a17fc26884efdf8b91f73dbedc9cfc20
oliviatroitino <olivia.troitino@live.u-tad.com>
2025-06-20 14:30:24 +0200

    agrega validador user data

--- src/validators/user.js
+++ src/validators/user.js
@@ -10,4 +10,22 @@ const validatorRegisterUser = [
     }
 ];
 
-module.exports = { validatorRegisterUser }
+const validatorLogin = [
+  check("email").exists().notEmpty().isEmail(),
+  check("password").exists().notEmpty(),
+  (req, res, next) => validateResults(req, res, next)
+];
+
+const validatorUserData = [
+  check("name").exists().notEmpty(),
+  check("surnames").exists().notEmpty(),
+  check("nif").exists().notEmpty(),
+  check("address.street").exists().notEmpty(),
+  check("address.number").exists().notEmpty().isNumeric(),
+  check("address.postal").exists().notEmpty().isNumeric(),
+  check("address.city").exists().notEmpty(),
+  check("address.province").exists().notEmpty(),
+  (req, res, next) => validateResults(req, res, next)
+];
+
+module.exports = { validatorRegisterUser, validatorLogin, validatorUserData }
