07f2ac0109c14797ed3d7e2189a36c3188e4292c
oliviatroitino <olivia.troitino@live.u-tad.com>
2025-06-20 21:51:21 +0200

    update

--- src/routes/project.js
+++ src/routes/project.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middleware/session");
-const { validatorCreateProject, validatorGetProjects } = require("../validators/project");
+const { validatorCreateProject, validatorGetProjects, validatorGetProject } = require("../validators/project");
 const { createProject, getProjects } = require("../controllers/project");
 
 router.post("/", authMiddleware, validatorCreateProject, createProject);
 router.get("/", authMiddleware, validatorGetProjects, getProjects);
+// router.get("/:id", authMiddleware, validatorGetProject, );
 
 module.exports = router;
