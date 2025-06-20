c2c1634af0b37fb11c7d182c41ccb19cfaecec0b
oliviatroitino <olivia.troitino@live.u-tad.com>
2025-06-20 15:14:53 +0200

    agrega getClientById

--- src/routes/client.js
+++ src/routes/client.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middleware/session");
-const { validatorCreateClient } = require("../validators/client.js");
-const { createClient, getClients } = require("../controllers/client.js");
+const { validatorCreateClient, validatorGetId } = require("../validators/client.js");
+const { createClient, getClients, getClientById } = require("../controllers/client.js");
 
 router.post("/", authMiddleware, validatorCreateClient, createClient);
 router.get("/", authMiddleware, getClients);
+router.get("/:id", authMiddleware, validatorGetId, getClientById);
 
 module.exports = router;
