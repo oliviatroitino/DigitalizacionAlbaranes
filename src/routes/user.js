82de2040de5ff70b653caaa435e5e07f8c2bd38c
oliviatroitino <olivia.troitino@live.u-tad.com>
2025-06-20 14:24:03 +0200

    corrige errores

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
 //// Validador
-const { validatorRegisterUser } = require("../validators/user");
+const { validatorRegisterUser, validatorLogin } = require("../validators/user");
+const { validatorEmailCode } = require("../validators/email");
 //// Controller
-const { registerUser } = require("../controllers/user");
+const { registerUser, verifyEmail, loginUser } = require("../controllers/user");
 
 router.post("/register", validatorRegisterUser, registerUser);
+router.put("/validation", validatorEmailCode, verifyEmail);
+router.post("/login", validatorLogin, loginUser);
 
 module.exports = router;
