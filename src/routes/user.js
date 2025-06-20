b580ce0346d677b077fb886020240ac01bf98986
oliviatroitino <olivia.troitino@live.u-tad.com>
2025-06-20 14:35:41 +0200

    agrega rutas patch de datos de usuario y compania

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,9 +5,12 @@ const { validatorRegisterUser, validatorLogin } = require("../validators/user");
 const { validatorEmailCode } = require("../validators/email");
 //// Controller
 const { registerUser, verifyEmail, loginUser } = require("../controllers/user");
+const authMiddleware = require("../middleware/session.js");
 
 router.post("/register", validatorRegisterUser, registerUser);
 router.put("/validation", validatorEmailCode, verifyEmail);
 router.post("/login", validatorLogin, loginUser);
+router.patch("/", authMiddleware, validatorUpdateUser, updateUser);
+router.patch("/company", authMiddleware,  validatorCompany, updateCompany);
 
 module.exports = router;
