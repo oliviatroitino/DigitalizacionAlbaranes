63b2ed41d211f07c44fe4c9acf869b3b36b89d26
oliviatroitino <olivia.troitino@live.u-tad.com>
2025-06-20 15:33:33 +0200

    agrega deleteClient

--- src/routes/client.js
+++ src/routes/client.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middleware/session");
 const { validatorCreateClient, validatorGetId, validatorUpdateClient } = require("../validators/client.js");
-const { createClient, getClients, getClientById, updateClient } = require("../controllers/client.js");
+const { createClient, getClients, getClientById, updateClient, deleteClient } = require("../controllers/client.js");
 
 router.post("/", authMiddleware, validatorCreateClient, createClient);
 router.get("/", authMiddleware, getClients);
 router.get("/:id", authMiddleware, validatorGetId, getClientById);
 router.patch("/:id", authMiddleware, validatorUpdateClient, updateClient);
+router.delete("/:id", authMiddleware, validatorGetId, deleteClient);
 
 module.exports = router;
