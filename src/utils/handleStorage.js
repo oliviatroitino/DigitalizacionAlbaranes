520f3454ddc76e652374cb3b1e6eaeadeec960f6
oliviatroitino <olivia.troitino@live.u-tad.com>
2025-07-02 23:40:04 +0200

    update

--- src/utils/handleStorage.js
+++ src/utils/handleStorage.js
@@ -1,13 +1,12 @@
 const multer = require('multer')
 
 const storage = multer.diskStorage({
-    destination:function(req, file, callback){ // pasan argumentos automaticamente
+    destination:function(req, file, callback){
         const pathStorage = __dirname+"/../storage"
-        callback(null, pathStorage) // error y destination
+        callback(null, pathStorage)
     },
-    filename:function(req, file, callback){ // sobreescribimos o renombramos
-        // tienen extension jpg, pdf, mp4
-        const ext = file.originalname.split(".").pop() // el ultimo valor
+    filename:function(req, file, callback){
+        const ext = file.originalname.split(".").pop()
         const filename = "file-"+Date.now()+"."+ext
         callback(null, filename)
     }
