529b3fc22d74fdb6a551013a9b358d848abd46c4
oliviatroitino <olivia.troitino@live.u-tad.com>
2025-07-07 09:57:39 +0200

    updates post fecha de entrega, logs slack

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const setupSwagger = require('./swagger');
+const loggerStream = require("./src/utils/handleLogger")
+const morganBody = require("morgan-body")
 
 //////// Routers y conexión a MongoDB
 const dbConnect = require('./src/config/mongo');
@@ -12,12 +14,21 @@ const deliveryNoteRouter = require('./src/routes/delivery-note.js');
 
 app.use(cors())
 app.use(express.json());
-
+morganBody(app, {
+    noColors: true,
+    skip: function(req, res) {
+        return res.statusCode < 400
+    },
+    stream: loggerStream
+})
 //////// Rutas
 app.use('/api/user', userRouter);
 app.use('/api/client', clientRouter);
 app.use('/api/project', projectRouter);
 app.use('/api/deliverynote', deliveryNoteRouter);
+app.get('/test-error', (req, res) => {
+    res.status(500).send('Error de prueba');
+});
 
 setupSwagger(app);
 
--- src/utils/handleError.js
+++ src/utils/handleError.js
@@ -1,4 +1,12 @@
+const loggerStream = require('./handleLogger');
+
 const handleHttpError = (res, message, code = 403) => {
+
+    if (code >= 500 && code < 600) {
+        const formatted = `[${code}] ${message}`;
+        loggerStream.write(formatted);
+    }
+
     res.status(code).send(message);
 };
 
